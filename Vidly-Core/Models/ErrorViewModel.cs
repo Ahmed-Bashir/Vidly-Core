7eb53f514d937e4398f10c79ed69e6486e6c6b14
Ahmed Salad <Ahmed.Salad@brightstar.com>
2019-06-17 16:43:47 +0100

    Add project files.

---
+++ Vidly-Core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Vidly_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
